60da731cd419c6e047478a7b9c7ddf765f2af39b
menna12999 <mennaabdelaziz129@gmail.com>
2025-10-06 05:44:43 +0300

    fifth commit

--- src/middleware.ts
+++ src/middleware.ts
@@ -14,6 +14,6 @@ export async function middleware(request: NextRequest) {
 }
  
 // See "Matching Paths" below to learn more
-export const config = {
-  matcher: '/',
-}
+// export const config = {
+//   matcher: '/',
+// }
